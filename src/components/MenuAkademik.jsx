d7047bf1778fa1ec180295ba6ae786ba60f8b275
faizbyp <faizbyp@gmail.com>
2023-05-20 20:16:51 +0700

    add akademik page

---
+++ src/components/MenuAkademik.jsx
@@ -0,0 +1,47 @@
+import MenuImg from "../assets/img/menu.png";
+
+function MenuAkademik() {
+  const data = [
+    {
+      title: "Peraturan Akademik",
+      link: "/"
+    },
+    {
+      title: "Beasiswa",
+      link: "/"
+    },
+    {
+      title: "Jadwal Ujian",
+      link: "/"
+    },
+    {
+      title: "Timeline Akademik",
+      link: "/"
+    },
+    {
+      title: "Jadwal Kuliah",
+      link: "/"
+    },
+  ]
+
+  return (
+    <>
+      <div className="row justify-content-center g-4">
+        {data && data.map((item) => (
+          <div className="col-12 col-md-4">
+            <div class="card">
+              <img src={MenuImg} class="card-img-top object-fit-cover" alt="..." height={300} />
+              <div class="card-body">
+                <h5 class="card-title my-3">{item.title}</h5>
+                <div className="text-end">
+                  <a href={item.link} class="btn btn-primary rounded-pill px-3">See more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+export default MenuAkademik
