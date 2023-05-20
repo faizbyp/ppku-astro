6a704a3e43140a32c054cdd7f6627cf200939320
faizbyp <faizbyp@gmail.com>
2023-05-20 20:19:39 +0700

    fix cards

--- src/components/MenuAkademik.jsx
+++ src/components/MenuAkademik.jsx
@@ -29,12 +29,12 @@ function MenuAkademik() {
       <div className="row justify-content-center g-4">
         {data && data.map((item) => (
           <div className="col-12 col-md-4">
-            <div class="card">
+            <div class="card border-black">
               <img src={MenuImg} class="card-img-top object-fit-cover" alt="..." height={300} />
               <div class="card-body">
                 <h5 class="card-title my-3">{item.title}</h5>
                 <div className="text-end">
-                  <a href={item.link} class="btn btn-primary rounded-pill px-3">See more</a>
+                  <a href={item.link} class="btn btn-primary rounded-pill px-3 mb-3">See more</a>
                 </div>
               </div>
             </div>
